29689bf33864ca2abc149289b2dcaf3c9540e640
Gary Thickett <gaz@brainmonkey.co.uk>
2012-06-28 14:36:58 +0100

    Added references and IRepository interface

---
+++ Source/Repository/IRespository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bullet.Repository {
+
+    /// <summary>
+    /// Main Interface Definition
+    /// </summary>
+    /// <typeparam name="T">The object type of this repository</typeparam>
+    public interface IRespository<T> {
+    }
+}
