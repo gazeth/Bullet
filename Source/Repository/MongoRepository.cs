8c22ef5bc16b614358d68bd84c98abaa12fcbfa3
Gary Thickett <gaz@brainmonkey.co.uk>
2012-06-28 15:04:42 +0100

    Added MongoRepo class from my laptop

---
+++ Source/Repository/MongoRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bullet.Repository {
+
+    /// <summary>
+    /// Concrete implementation of IRepository
+    /// </summary>
+    /// <typeparam name="T">The object type of this repository</typeparam>
+    public class MongoRepository<T> : IRespository<T> {
+    }
+}
